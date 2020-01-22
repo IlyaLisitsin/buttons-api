4d57e1771f52031e0d3d1b03ad8911b1af54fbe7
Illia <ilyas.sporichev@gmail.com>
2020-01-22 18:21:40 +0300

    upload to aws bucket restored
    upload to aws bucket restored

--- services/upload-to-bucket.js
+++ services/upload-to-bucket.js
@@ -1,7 +1,5 @@
 const { httpGet } = require('./helpers');
 
-const { SavedUser } = require('../models');
-
 async function getFileUrl(fileId) {
     const fileRequest = await httpGet(`https://api.telegram.org/bot${process.env.TG_TOKEN}/getFile?file_id=${fileId}`);
     const fileUrl = await JSON.parse(fileRequest).result.file_path;
@@ -20,33 +18,33 @@ const s3 = new AWS.S3({
 const uploadToBucket = async function ({ audioId, avatarId, username }) {
     const audioUrl = await getFileUrl(audioId);
     const avatarUrl = await getFileUrl(avatarId);
+    const audioFileContent = await httpGet(`https://api.telegram.org/file/bot${process.env.TG_TOKEN}/${audioUrl}`);
+    const avatarFileContent = await httpGet(`https://api.telegram.org/file/bot${process.env.TG_TOKEN}/${avatarUrl}`);
 
-    // const fileUrl = await JSON.parse(fileRequest).result.file_path;
-    const fileContent = await httpGet(`https://api.telegram.org/file/bot${process.env.TG_TOKEN}/${audioUrl}`);
-
-    const params = {
-        Bucket: 'button-audio',
-        Key: username,
-        Body: fileContent
-    };
-
-    s3.upload(params, function(err, data) {
-
-
-        if (err) console.log(err);
-        else console.log(`File uploaded successfully. ${data.Location}`);
-    });
-
-    SavedUser.find({ username }).then((data) => {
-        const newSavedUser = { audioUrl, avatarUrl, username };
-        if (!data.length) {
-            new SavedUser(newSavedUser).save()
-                .then(() => console.log(`Username "${username}" successfully added`));
-        } else {
-            SavedUser.updateOne({ username }, { audioUrl, avatarUrl }, { new: true })
-                .then(() => console.log(`Username "${username}" successfully updated`));
-        }
-    }).catch(err => console.log(`User upload failed with ${err}`));
+    await Promise.all([
+        new Promise(resolve => s3.upload({
+            Bucket: 'button-avatar',
+            Key: `${username}.jpg`,
+            Body: avatarFileContent,
+        }, function(err, data) {
+            if (err) console.log(err);
+            else {
+                console.log(`Avatar of "${username}" uploaded successfully. ${data.Location}`);
+                resolve();
+            }
+        })).catch(err => console.log(`Avatar upload of "${username}" failed with ${err}`)),
+        new Promise(resolve => s3.upload({
+            Bucket: 'button-audio',
+            Key: `${username}.ogg`,
+            Body: audioFileContent,
+        }, function(err, data) {
+            if (err) console.log(err);
+            else {
+                console.log(`Voice of "${username}" uploaded successfully. ${data.Location}`);
+                resolve();
+            }
+        })).catch(err => console.log(`Voice upload of "${username}" failed with ${err}`))
+    ]);
 };
 
 module.exports = { uploadToBucket };
